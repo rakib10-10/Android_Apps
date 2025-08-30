d4a4f089ef9108eb3c8f94b3e020f9a41ae95a26
rakib <mdtara0199@gmail.com>
2025-08-30 23:27:18 +0600

    Basic Web View added

---
+++ Web View/app/src/main/java/com/example/web_view/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.web_view;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://github.com/rakib10-10");
+
+    }
+}
